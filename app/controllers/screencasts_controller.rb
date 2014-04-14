208a779720d90578609177da199fe763f70971cb
James Newman <jamesmnewman1@gmail.com>
2014-04-13 22:12:56 -0700

    controller/json

--- app/controllers/screencasts_controller.rb
+++ app/controllers/screencasts_controller.rb
@@ -1,2 +1,14 @@
 class ScreencastsController < ApplicationController
+
+	# Get screencasts
+	# Get screencasts.json
+	def index
+		render json: Screencast.all
+	end
+
+	#get screencasts id
+	#get screencasts id json
+	def show
+		render json: Screencast.find(params[:id])
+	end
 end
