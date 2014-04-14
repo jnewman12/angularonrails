d10bdde5a9e61d6a7eec743f54275d9be1ff90d6
James Newman <jamesmnewman1@gmail.com>
2014-04-13 22:37:01 -0700

    sexy angular

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Angularonrails::Application.routes.draw do
+  get "home/index"
   scope :api do
     get "/screencasts(.:format)" => "screencasts#index"
     get "/screencasts/:id(.:format)" => "screencasts#show"
   end
+
+  root to: "home#index"
 end
