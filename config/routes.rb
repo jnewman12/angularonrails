dac7766670f754d4aed9a3917e65abb91009225d
James Newman <jamesmnewman1@gmail.com>
2014-04-13 22:37:38 -0700

    sneaky generators

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Angularonrails::Application.routes.draw do
-  get "home/index"
   scope :api do
     get "/screencasts(.:format)" => "screencasts#index"
     get "/screencasts/:id(.:format)" => "screencasts#show"
