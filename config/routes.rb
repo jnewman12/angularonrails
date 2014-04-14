7a651c6104d7b186f2c2bfa3c133dc2317715a03
James Newman <jamesmnewman1@gmail.com>
2014-04-13 22:10:37 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Angularonrails::Application.routes.draw do
-  resources :screencasts
+  scope :api do
+    get "/screencasts(.:format)" => "screencasts#index"
+    get "/screencasts/:id(.:format)" => "screencasts#show"
+  end
 end
