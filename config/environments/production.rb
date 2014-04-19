e2de1a4eb03db445897ba387ada123ff4cb04596
James Newman <jamesmnewman1@gmail.com>
2014-04-19 11:02:03 -0700

    errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,4 @@ Angularonrails::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
-  config.assets.js_compressor = Sprockets::LazyCompressor.new { Uglifier.new(mangle: false)}
 end
